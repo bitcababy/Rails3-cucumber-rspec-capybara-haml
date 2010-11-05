3ea42cdb9e7b49e3e75ed6182f8276890d195726
Meredith Lesly <bitcababy+github@gmail.com>
2010-11-05 13:35:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zeroedin::Application.config.secret_token = '44601ecfcacf02c693238a3be85a3da586ebec13ddb87ef76344a77448bb52509810db8122be5c8b6fee4dbd61ceb296deb4235994c37585c78eb529fecc0ec4'
